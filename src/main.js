88ed68de40fb0b8831b753670f070e5e8dfb3665
xfbaby <852527321@qq.com>
2018-05-30 10:52:47 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,13 +1,10 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+
 require('es6-promise').polyfill()       
 import Vue from 'vue'
 import App from './App'
 import router from './router'
 import 'babel-polyfill'
-// import 'promise-polyfill/src/polyfill'
 import '@/plugins/flexible'
-import MintUI from 'mint-ui'
 import store from '@/store'
 import FastClick from 'fastclick'
 import '@/utils/directives'
@@ -22,7 +19,6 @@ if ('addEventListener' in document) {
     }, false);
 }
 
-Vue.use(MintUI)
 Vue.use(VueScroller)
 Vue.use(VueLazyLoad)
 Vue.use(VeeValidate)
